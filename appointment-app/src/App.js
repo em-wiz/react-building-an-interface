4e8ff63ea73755e5924ab65d466aa6c399d69b25
Wisdom Emmanuel <emmawhiz.95@gmail.com>
2022-08-10 09:34:21 +0100

    add search, dropdown & add appointment components

--- appointment-app/src/App.js
+++ appointment-app/src/App.js
@@ -1,11 +1,17 @@
-import { BiArchive } from "react-icons/bi";
+import { BiCalendar } from "react-icons/bi";
+import AddAppointment from "./components/AddAppointment";
+import Search from "./components/Search";
+
+
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top" /> Your Appointments
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-400 align-top" /> Your Appointments
       </h1>
+      <AddAppointment />
+      <Search />
     </div>
   );
 }
