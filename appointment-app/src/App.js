8ecf2bd6e69cfe82c4a4ad7f2613259a8f87e7d9
Wisdom Emmanuel <emmawhiz.95@gmail.com>
2022-08-13 05:04:16 +0100

    move data.json to public, add AppointmentInfo component, add fetchData function to fetch json data from public using useCallback & useEffect to keep track of changes

--- appointment-app/src/App.js
+++ appointment-app/src/App.js
@@ -1,10 +1,28 @@
-import { BiCalendar, BiTrash } from "react-icons/bi";
+import { useState, useEffect, useCallback } from "react"
+import { BiCalendar } from "react-icons/bi";
 import AddAppointment from "./components/AddAppointment";
 import Search from "./components/Search";
-import appointmentList from "./components/data.json"
+import AppointmentInfo from "./components/AppointmentInfo";
 
 
 function App() {
+
+  let [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(reponse => reponse.json())
+      .then(data => {
+        setAppointmentList(data)
+      });
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData]);
+
+
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
@@ -12,25 +30,14 @@ function App() {
       </h1>
       <AddAppointment />
       <Search />
-      <ul>
+      <ul className="divide-y divide-gray-200">
         {appointmentList.map(appointment => (
-          <li className="px-3 py-3 flex items-start">
-          <button type="button"
-            className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-            <BiTrash /></button>
-          <div className="flex-grow">
-            <div className="flex items-center">
-              <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
-              <span className="flex-grow text-right">{appointment.aptDate}</span>
-            </div>
-            <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
-            <div className="leading-tight">{appointment.aptNotes}</div>
-          </div>
-        </li>
+          <AppointmentInfo key={appointment.id} appointment={appointment} />
         ))}
       </ul>
     </div>
   );
 }
+
 
 export default App;
